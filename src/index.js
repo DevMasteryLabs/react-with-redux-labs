073199bcfa5326fe7f275355fcffd475d07a2434
DevMasteryLabs <develop.devmastery@gmail.com>
2021-07-11 17:06:00 +0100

    Provide Redux Store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -11,7 +13,9 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
